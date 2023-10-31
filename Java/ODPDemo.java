d4a41b79d1b68bd798795df714dd2f6b1280f5f5
nimamasoumi <mraghahe@protonmail.com>
2023-10-31 17:44:18 -0400

    Making a user interactive app for the observer design pattern

--- Java/ODPDemo.java
+++ Java/ODPDemo.java
@@ -1,25 +1,62 @@
+import java.util.Scanner;
 class ODPDemo
 {
     public static void main(String[] args)
     {
+        var userC = new Scanner(System.in);
+
         var subject1 = new Subject();
-        var observer1 = new ConcreteObserver("observer1");
-        var observer2 = new ConcreteObserver("observer2");
-
-        subject1.attachO(observer1);
-        subject1.attachO(observer2);
-
         subject1.setState("initialization");
         subject1.notifyO();
 
-        subject1.setState("running");
-        subject1.notifyO();
+        String _command, _name;
+        boolean exitApp = false;
+        while(!exitApp)
+        {
+            System.out.println("\nEnter a command: add/state/remove/dispose");
+            _command = userC.nextLine();
+            if(_command.equals("dispose"))
+            {
+                subject1.setState("disposing");            
+                subject1.notifyO();
+                subject1.disposeAll();                
+                exitApp = true;
+            }
+            else if(_command.equals("add"))
+            {
+                System.out.println("Adding an observer, enter its name: ");
+                subject1.attachO((new ConcreteObserver(userC.nextLine())));
 
-        subject1.setState("disposing");
-        subject1.notifyO();
-        subject1.detachO(observer1);
-
-        subject1.notifyO();
-        subject1.detachO(observer2);
-    }
+                subject1.setState("running");
+                subject1.notifyO();
+            }
+            else if(_command.equals("remove"))
+            {
+                System.out.println("Removing an observer, enter its name: ");
+                _name = userC.nextLine();
+                var it = subject1.getObservers().listIterator();
+                while (it.hasNext())
+                {
+                    var obs = (ConcreteObserver) it.next();
+                    if(_name.equals(obs.getName()))
+                    {
+                        subject1.detachO(obs);
+                        break;
+                    }
+                }                
+                subject1.setState("running");
+                subject1.notifyO();
+            }
+            else if(_command.equals("state"))
+            {
+                System.out.println("Setting the state, enter the state: ");
+                subject1.setState(userC.nextLine());
+                subject1.notifyO();
+            }
+            else
+            {
+                System.out.println(_command+" is not a valid command!\n");                
+            }
+        }                                        
+    }        
 }
