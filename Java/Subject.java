85444a062a87057e5937c73e755e310a03528b10
nimamasoumi <mraghahe@protonmail.com>
2023-10-31 17:44:54 -0400

    Adding a dispose method

--- Java/Subject.java
+++ Java/Subject.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 
 class Subject
 {
-    List<IObserver> observers = new ArrayList<>();
+    private List<IObserver> observers = new ArrayList<>();
     private String state;
     public void notifyO()
     {
@@ -13,14 +13,14 @@ class Subject
             it.next().update(this.getState());
         }
     }
-    public void attachO(IObserver _o)
+    public synchronized void attachO(IObserver _o)
     {
         this.observers.add(_o);
     }
-    public void detachO(IObserver _o)
+    public synchronized void detachO(IObserver _o)
     {
         this.observers.remove(_o);
-    }
+    }  
     public String getState()
     {
         return this.state;
@@ -29,4 +29,16 @@ class Subject
     {
         this.state=_state;
     } 
+    public List<IObserver> getObservers()
+    {
+        return this.observers;
+    }
+    public synchronized void disposeAll()
+    {
+        var it = observers.listIterator();
+        while (it.hasNext())
+        {
+            this.observers.remove(it.next());
+        }        
+    }
 }
